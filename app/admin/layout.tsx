1e567a6960aef6d9a565dda0ea46a24301c3d896
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-06-01 22:16:44 -0600

    ssss

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
     <>
       <AdminNav />
       <div className="lg:min-h-screen container mx-auto mt-10 px-10 lg:px-0">
-        <div className="bg-white shadow w-full  mx-auto p-10 my-10 lg:w-3/5">
+        <div className="bg-white shadow w-full mx-auto p-10 my-10 lg:w-3/5">
           {children}
         </div>
       </div>
